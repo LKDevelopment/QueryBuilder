08e54ef6b4033fdf8c838b99ce9305ac62de9d7f
GMacx3 <g-mac@gmx.de>
2014-12-18 19:39:45 +0100

    Homepage angepasst

--- index.php
+++ index.php
@@ -27,8 +27,8 @@ require_once 'QueryBuilder.class.php';
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="windows-1252">
-        <title></title>
+        <meta charset="utf-8">
+        <title>Query Builder by LK-Development</title>
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 
@@ -43,7 +43,49 @@ require_once 'QueryBuilder.class.php';
 
     </head>
     <body>
-        <h3>SELECT Abfragen</h3>
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="container-fluid">
+                <!-- Titel und Schalter werden für eine bessere mobile Ansicht zusammengefasst -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Navigation ein-/ausblenden</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Query Builder by LK-Development</a>
+                </div>
+
+                <!-- Alle Navigationslinks, Formulare und anderer Inhalt werden hier zusammengefasst und können dann ein- und ausgeblendet werden -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#info">INFO</a></li>
+                        <li><a href="#select">SELECT</a></li>
+                        <li><a href="#insert">INSERT INTO</a></li>
+                        <li><a href="#update">UPDATE</a></li>
+                        <li><a href="#free">Freier Query</a></li>
+                        <li><a href="#other">Andere</a></li>
+                        <li><a href="http://querybuilder.lk-development.de/doku">API Doku</a></li>
+                        <li><a href="https://github.com/GMacx3/QueryBuilder">Download (auf Github)</a></li>
+                        <li><a href="https://www.lukas-kaemmerling.de/?page=legalnotice" target="_blank">Impressum</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+        <h3><a name="info">INFO</a></h3>
+        <pre>
+            Wer kennt es nicht? Man hat einen ellen langen SQL Query, er wird so lang das man ihn nicht mehr auf einen Blick versteht. 
+            Um diesem Entgegen zu setzen gibt es sogenannte OR-Mapper (object-relational mapping, kurz: ORM). Sie lassen schwere Query Konstrukte einfacher dastellen.
+            Da ein ganzer ORM Mapper viel mehr funktionen hat als nur das einfache erstellen von Query, habe ich mir einen QueryBuilder geschrieben.
+            So lässt sich einfach und verständlich, riesen große Query Konstrukte bauen. Wie das ganze Funktioniert sehen Sie weiter unten. 
+
+            Dies ist ein freies Projekt. Die Nutzung ist unter der MIT Lizenz gewährt.
+            Aktuelle Version : 1.0.0
+            Mindest PHP Version: 5.3.X
+        </pre>
+        <hr>
+        <h3><a name="select">SELECT Abfragen</a></h3>
+
         <pre>
         <label>Quellcode</label>
             <code class="php">
@@ -55,15 +97,19 @@ require_once 'QueryBuilder.class.php';
         </pre>
         <label>Ergebnis:</label>
         <samp> <?php
-            
             $queryBuilderSELECT = new lkdevelopment\QueryBuilder();
-            $queryBuilderSELECT->select("bestellungID", "bestellungDatum")->from("bestellungen", "b")->where("b.bestellungID = :bid")->groupBy("b.bestellDatum")->limit(1, 3);
+            $queryBuilderSELECT->select("b.bestellungID", "b.bestellungDatum","p.p_name")
+                    ->from("bestellungen", "b")
+                    ->innerJoin("praemie", "p", "b.p_id = p.p_id")
+                    ->where("b.bestellungID = :bid")
+                    ->orWhere("b.bestellungZeitraum > :timestampGestern");
             echo $queryBuilderSELECT;
             ?></samp>
 
 
         <hr>
-        <h3>INSERT INTO</h3>
+        <h3><a name="insert">INSERT INTO</a></h3>
+
         <pre>
         <label>Quellcode</label>
             <code class="php">
@@ -82,7 +128,7 @@ require_once 'QueryBuilder.class.php';
 
 
         <hr>
-        <h3>UPDATE</h3>
+        <h3><a name="update">UPDATE</a></h3>
         <pre>
         <label>Quellcode</label>
             <code class="php">
@@ -99,24 +145,7 @@ require_once 'QueryBuilder.class.php';
             ?></samp>
 
         <hr>
-        <h3>UPDATE</h3>
-        <pre>
-        <label>Quellcode</label>
-        <code class="php">
-        $queryBuilderDelete = new lkdevelopment\QueryBuilder();
-        $queryBuilderDelete->deleteFrom("bestellungen")->where("bestellungDatem > :bDatum")->limit(2);
-        echo $queryBuilderDelete;
-            </code>
-        </pre>
-        <label>Ergebnis:</label>
-        <samp><?php
-            $queryBuilderDelete = new lkdevelopment\QueryBuilder();
-            $queryBuilderDelete->deleteFrom("bestellungen")->where("bestellungDatem > :bDatum")->limit(2);
-            echo $queryBuilderDelete;
-            ?></samp>
-
-        <hr>
-        <h3>Freier Query</h3>
+        <h3><a name="free">Freier Query</a></h3>
         <pre>
         <label>Quellcode</label>
         <code class="php">
@@ -126,10 +155,12 @@ require_once 'QueryBuilder.class.php';
             </code>
         </pre>
         <label>Ergebnis:</label>
-        <samp><?php
+        <samp>
+            <?php
             $queryBuilderFree = new lkdevelopment\QueryBuilder();
             $queryBuilderFree->query("TRUNCATE `bestellungen`");
             echo $queryBuilderFree;
-            ?></samp>
+            ?>
+        </samp>
     </body>
 </html>
